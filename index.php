49e39a167365a4a8e02f59462736d3975f87cd18
hishamump <49261241+hishamump@users.noreply.github.com>
2019-12-03 13:10:55 +0800

    Index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>UBung Food Service</title>
+</head>
+<?php include 'dataConnection.php';?>
+<body style="background-image: url('image/2.jpg'); background-repeat: no-repeat;">
+    <?php include 'menu.php';?>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,10 @@
+<div style="background-color: lightblue;">
+<a href="index.php">Home</a> | 
+<a href="index.php">Despatcher</a> | 
+<a href="index.php">Customer </a> | 
+<a href="index.php">Order </a> | 
+<a href="index.php">Restaurant/Cafeteria</a> | 
+<a href="announcement.php">Announcement</a> | 
+<a href="report.php">reports</a> | 
+
+</div>
