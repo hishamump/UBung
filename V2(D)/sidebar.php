dbe5f826870370b6314de58ec52fdb6fe47b7f4c
Hisham <49261241+hishamump@users.noreply.github.com>
2019-12-10 12:10:02 +0800

    Fix sidebar

--- V2(D)/sidebar.php
+++ V2(D)/sidebar.php
@@ -1,9 +1,32 @@
 <ul class="sidebar navbar-nav">
     <li class="nav-item active">
-    <a class="nav-link" href="new.php">
+    <a class="nav-link" href="index.php">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span>
     </a>
+    </li>
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Announcements</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+        <a class="dropdown-item" href="aList.php">List</a>
+        <a class="dropdown-item" href="aAdd.php">Add New</a>
+    </div>
+    </li>
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-fw fa-folder"></i>
+        <span>Reports</span>
+    </a>
+    <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+        <a class="dropdown-item" href="rDespatchers.php">Despatchers</a>
+        <a class="dropdown-item" href="rCustomers,.php">Customers</a>
+
+        <a class="dropdown-item" href="rOrders.php">Orders</a>
+        <a class="dropdown-item" href="rRegistered,.php">Restaurants/Cafeterias</a>        
+    </div>
     </li>
     <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -12,15 +35,23 @@
     </a>
     <div class="dropdown-menu" aria-labelledby="pagesDropdown">
         <h6 class="dropdown-header">Login Screens:</h6>
-        <a class="dropdown-item" href="login.php">Login</a>
-        <a class="dropdown-item" href="register.php">Register</a>
+        <a class="dropdown-item" href="login.html">Login</a>
+        <a class="dropdown-item" href="register.html">Register</a>
         <a class="dropdown-item" href="forgot-password.html">Forgot Password</a>
         <div class="dropdown-divider"></div>
         <h6 class="dropdown-header">Other Pages:</h6>
         <a class="dropdown-item" href="404.html">404 Page</a>
-		<a class="dropdown-item" href="despatcher.php">Despatcher Page</a>
         <a class="dropdown-item" href="blank.html">Blank Page</a>
     </div>
     </li>
-    
+    <li class="nav-item">
+    <a class="nav-link" href="charts.php">
+        <i class="fas fa-fw fa-chart-area"></i>
+        <span>Charts</span></a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="tables.php">
+        <i class="fas fa-fw fa-table"></i>
+        <span>Tables</span></a>
+    </li>
 </ul>
