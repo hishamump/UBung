1e9c80c5729542deb01904d54ce6a55fdf3ccc37
Hisham <49261241+hishamump@users.noreply.github.com>
2019-12-10 13:50:41 +0800

    Add reports pages

---
+++ reports/customers,.php
@@ -0,0 +1,3 @@
+<?php include '../header.php';?>
+  <h2>Customers</h2>
+<?php include '../footer.php';?>
---
+++ reports/registered,.php
@@ -0,0 +1,3 @@
+<?php include '../header.php';?>
+  <h2>Registered Restaurants/Cafeterias</h2>
+<?php include '../footer.php';?>
