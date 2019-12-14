4745f47dd1d168c22fb587c0af381c87a6d7bae8
Hisham <49261241+hishamump@users.noreply.github.com>
2019-12-14 19:53:46 +0800

    testUpload.php

---
+++ testUpload.php
@@ -0,0 +1,69 @@
+<?php
+include 'constants.php';
+$target_dir = $_SERVER['DOCUMENT_ROOT'] . "/" . ROOT . "/uploads//"; //"uploads/";
+// Check if image file is a actual image or fake image
+if (isset($_POST["submit"]) && $_FILES["fileToUpload"]["tmp_name"] != "") {
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if (
+        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif"
+    ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+    } else {
+        echo "<br>";
+        echo "__FILE__" . __FILE__;
+         echo "<br>";
+        echo "__DIR__" . __DIR__;
+         echo "<br>";
+        echo "DOCUMENT_ROOT" . $_SERVER['DOCUMENT_ROOT'];
+       echo "<br>";
+        echo "target_dir" . $target_dir;
+       echo "<br>";
+        echo $target_file;
+                
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+</body>
+
+</html>
