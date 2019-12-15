edf478030d3964295e023e83b6541e963974f26c
Tongzwei <58357790+Tongzwei@users.noreply.github.com>
2019-12-15 12:26:58 +0800

    Update readFoodForm.php

--- Restaurant/NEW Restaurant/readFoodForm.php
+++ Restaurant/NEW Restaurant/readFoodForm.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Enter Restaurant Data</title>
+    <title>Enter Your Order Data</title>
 </head>
 <?php include 'dataConnection.php';?>
 <body> 
