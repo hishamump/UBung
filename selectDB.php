3c77fb4832123bce05f696f74c7d01dc6d48ecb6
Hisham <49261241+hishamump@users.noreply.github.com>
2019-12-15 15:47:11 +0800

    Fix commands

--- selectDB.php
+++ selectDB.php
@@ -1,8 +1,9 @@
 <?php
+	include 'dbName.php';
  	include 'dataConnection.php';
 
 	// to select the targeted database
-	mysqli_select_db($link, "ubung") or die(mysqli_error());
+	mysqli_select_db($link, DBNAME) or die(mysqli_error($link));
 	
 	if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
@@ -10,4 +11,3 @@
  
 // Attempt select query execution with order by clause
 $sql = "SELECT * FROM user " ;
-?>
