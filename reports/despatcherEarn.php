09ea829e7f657ece3816c5d1f502a9fca3d6d3cf
Hisham <49261241+hishamump@users.noreply.github.com>
2019-12-16 13:29:02 +0800

    Include sql query in DespatcherEarn

--- reports/despatcherEarn.php
+++ reports/despatcherEarn.php
@@ -3,8 +3,23 @@
       CheckRole($roles) ?>
 <h2>Despatcher Earn</h2>
 <?php
-    $query = "SELECT `Id`, `UserName`, `Password`, `Address`, `Phone`, `Email`, `Role`, `Status` 
-              FROM `user` WHERE Role ='" . DESPATCHER . "'" or die(mysqli_connect_error());
+
+$did = 0;
+if (isset($_GET['id'])) {
+    $did = $_GET["id"];
+}
+    $query = "SELECT o.Id oid, `UserId`, `RestaurantId`, `DespatcherId`, o.`Status`, `OrderDate`, `DeliveryDate`,
+              r.Id rid, RName,
+              d.Id did, u.UserName DName,
+              u.Id uid, u.UserName UName,
+              SUM(od.Price) Total
+              FROM `orders` o 
+              LEFT JOIN restaurant r ON o.RestaurantId = r.Id
+              LEFT JOIN user d ON o.DespatcherId = d.Id AND d.Role = 'Despatcher'
+              LEFT JOIN user u ON o.UserId = u.Id
+              LEFT JOIN OrderDetails od ON od.OrderId = o.Id
+              WHERE DespatcherId = $did
+              Group BY o.Id, DeliveryDate" or die(mysqli_connect_error());
     $result = mysqli_query($link, $query);
 ?>
 
@@ -21,14 +36,7 @@
       </div>
     </div>
 </div>
-<?php 
-    //Check whether the insert was successful or not
-    if (!$result) {
-        ErrorMessage("Query failed" . mysqli_error($link));
-    }
-    //Close the database connection
-    mysqli_close($link);
-?>
+
 
 <?php include '../footer.php';?>
 
@@ -37,4 +45,69 @@
   <script src="../vendor/chart.js/Chart.min.js"></script>
 
 
-  <script src="../js/demo/chart-bar-demo.js"></script>
+  <script>
+  // Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June"],
+            //TODO: Publish data here from server side
+            //     <php while ($row = mysqli_fetch_array($result)) { 
+            //     echo $row["DeliveryDate"] . ",";
+            // }],
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "rgba(2,117,216,1)",
+      borderColor: "rgba(2,117,216,1)",
+      data: [4215, 5312, 6251, 7841, 9821, 14984]
+            //TODO: Publish data here from server side
+            //       <php while ($row = mysqli_fetch_array($result)) { 
+            //     echo $row["Total"] . ",";
+            // }]
+      ,
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          display: true
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+  </script>
+<?php 
+    //Check whether the insert was successful or not
+    if (!$result) {
+        ErrorMessage("Query failed" . mysqli_error($link));
+    }
+    //Close the database connection
+    mysqli_close($link);
+?>
