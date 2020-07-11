91137ae302719d59c84e7f858b9e440e40b7f0f7
Hisham <49261241+hishamump@users.noreply.github.com>
2020-07-11 13:17:36 +0800

    Dispathcer - Fix update stattus

--- despatcher/despatcherMain.php
+++ despatcher/despatcherMain.php
@@ -20,58 +20,55 @@ $ll = $_SESSION['username'];?>
     $query = "SELECT * FROM user WHERE username = '$ll'; " or die(mysqli_connect_error());
 
     $result = mysqli_query($link, $query);
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        ?>
+        <div class="row">
+            <div class="col-md-2">
+                <h5>Name:</h5>
+            </div>
+            <div class="col-md-6">
+                <?php echo $row["UserName"] ?>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <h5>Email:</h5>
+            </div>
+            <div class="col-md-6">
+                <?php echo $row["Email"] ?>
+            </div>
+        </div>
 
-    //Loop the recordset $rs
-    echo "<table border='1'>";
-	
-    while ($row = mysqli_fetch_array($result)) {
-        echo '
-        
-        <tr>
-            <td>Name:</td>
-            <td>' . $row["UserName"] . '</td>
-        </tr>
-        <tr>
-        <td>Email:</td>
-        <td>' . $row["Email"] . '</td>
-        </tr>
-        <tr>
-            <td>Status:</td>
-            <td>' . $row["Status"] . '</td>
-        </tr>
-        <tr>
-            <td>Role:</td>
-            <td>' . $row["Role"] . '</td>
-        </tr>
-       
-        <tr>
-            <td colspan = "2" style="background-color:grey">&nbsp;</td>
-        </tr>   
-		<tr>
-            <td style="background-color:purple">
-                <button onclick="window.location.href = \'update.php?id=' . $row["Id"] . '\';">Edit</button>
-				<button onclick="window.location.href = \'delete.php?id=' . $row["Id"] . '\';">Delete</button>
-            </td>
-          
-        </tr>                 
-        <tr>
-            <td colspan = "2" style="background-color:grey">&nbsp;</td>
-        </tr>           
-        ';        
-        
+        <div class="row">
+            <div class="col-md-2">
+                <h5>Status:</h5>
+            </div>
+            <div class="col-md-6">
+                <?php echo $row["Status"] ?>
+            </div>
+        </div>  
+
+        <?php if (IsInRole(DESPATCHER)) { ?>
+        <p>
+            <button onclick="window.location.href = 'update.php?id=<?php echo $row["Id"] ?>';"> Edit</button>
+        </p>
+        <?php }else{?>
+        <p>
+            <button class="btn btn-primary" onclick="window.location.href = 'index.php';"> &lt; List</button>&nbsp;
+        </p>
+        <?php
+            }
+    } else {
+        ErrorMessage("Account not found");
     }
-    echo "</table>";
 
     //Check whether the insert was successful or not
     if (!$result) {
         die("Query failed" . mysqli_error($link));
     }
-
-
     //Close the database connection
-    mysqli_close($link);
-
-    
-    ?>
-  
+    mysqli_close($link);    
+?>
+ 
 <?php include '../footer.php';?>
--- despatcher/update.php
+++ despatcher/update.php
@@ -1,12 +1,13 @@
-<?php include '../header.php';?>
-  <!-- Breadcrumbs-->
-  <ol class="breadcrumb">
+<?php include '../header.php'; ?>
+<!-- Breadcrumbs-->
+<ol class="breadcrumb">
     <li class="breadcrumb-item">
-      <a href="../index.php">Dashboard</a>
+        <a href="../index.php">Dashboard</a>
     </li>
     <li class="breadcrumb-item active">OrderMain</li>
-  </ol>
- <body>
+</ol>
+
+<body>
     <?php
     $submit = '';
     if (isset($_POST['submit'])) {
@@ -15,27 +16,23 @@
     $uid = 0;
     if (isset($_GET['id'])) {
         $uid = $_GET["id"];
-    }    
-    if ($submit == 'Update'){
+    }
+    if ($submit == 'Update') {
         $uid = $_POST["id"];
         //Collect and save updateed information
-       
-    
-    
+
+
+
         $name = $_POST["name"];
         $email = $_POST["email"];
         $status = $_POST["status"];
-        $role = $_POST["role"];        
-        
-        $strSQL = "UPDATE user SET username='$name', email='$email', status='$status',role='$role' WHERE id=$uid";
-       
+
+        $strSQL = "UPDATE user SET username='$name', email='$email', status='$status' WHERE id=$uid";
+
         $result = mysqli_query($link, $strSQL);
-        if($result) 
-        {
-            echo("Data updated successfully");
-        }
-        else 
-        {
+        if ($result) {
+            echo ("Data updated successfully");
+        } else {
             die("Update failed" . mysqli_error($link));
         }
     }
@@ -45,69 +42,73 @@
         $query = "SELECT * FROM user WHERE id = $uid " or die(mysqli_connect_error());
 
         $result = mysqli_query($link, $query);
-        if (mysqli_num_rows($result) == 1){
+        if (mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
     ?>
-    <h3>Update <?php echo $row["UserName"] ?> Data</h3>
-    <form action="update.php" method="POST">
-        <table>
-            <tr>
-                <td>Name:</td>
-                <td><input type="text" name="name" value ='<?php  echo $row["UserName"]   ?>'></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><input type="text" name="email" value='<?php  echo $row["Email"] ?>'></td>
-            </tr>
-           
-			<tr>
-                <td>Role:</td>
-                <td><input type="text" name="role"   value='<?php  echo $row["Role"] ?>'></td>
-                    
-                </td>
-            </tr>
-			<tr>
-                <td>Availability:</td>
-                <td><input type="text" name="status"   value='<?php  echo $row["Status"] ?>'></td>
-                 <td>(1 - Available , 0 - Not Available)</td>   
-                </td>
-		
-            </tr>
-			 <tr>
-                <td><input type="submit" name="submit" value="Update"></td>
-                <td></td>
-            </tr>
-           
-        </table>
-        <input type="hidden" id="" name="id" value='<?php echo $row["Id"]?>'>
-    </form>
-    <?php 
-        }else{
+            <h3>Update <?php echo $row["UserName"] ?> Data</h3>
+            <form action="update.php" method="POST">
+
+                <div class="row">
+                    <div class="col-md-2">
+                        <h5>Name:</h5>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" name="name" value='<?php echo $row["UserName"]   ?>'>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <h5>Email:</h5>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="text" name="email" value='<?php echo $row["Email"] ?>'>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-2">
+                        <h5>Status:</h5>
+                    </div>
+                    <div class="col-md-6">
+                        <select name="status">
+                            <option value="">Select...</option>
+                            <option value="1" <?=$row['Status'] == '1' ? ' selected="selected"' : '';?>>Available</option>
+                            <option value="0" <?=$row['Status'] == '0' ? ' selected="selected"' : '';?>>Not Available</option>
+                        </select>
+                    </div>
+                </div>
+                <input type="submit" name="submit" value="Update">
+                <input type="hidden" id="" name="id" value='<?php echo $row["Id"] ?>'>
+            </form>
+    <?php
+        } else {
             echo '<p style="background-color: #F0CCC4">User not found</p>';
         }
-    }else{
+    } else {
         echo '<p style="background-color: #F0CCC4">Please provide user id</p>';
     }
 
-    function isChecked($value, $compare){
-        if ($value == $compare){
+    function isChecked($value, $compare)
+    {
+        if ($value == $compare) {
             return "checked";
-        }else{
+        } else {
             return "";
         }
-    }    
+    }
 
-    function isSelected($value, $compare, $word = ''){
-        if ($word == ''){
+    function isSelected($value, $compare, $word = '')
+    {
+        if ($word == '') {
             $word = 'selected';
         }
-        if (strpos($value, $compare) !== false){
+        if (strpos($value, $compare) !== false) {
             return $word;
-        }else{
+        } else {
             return "";
         }
-    } 
+    }
     mysqli_close($link);
     ?>
-  
-<?php include '../footer.php';?>
+
+    <?php include '../footer.php'; ?>
