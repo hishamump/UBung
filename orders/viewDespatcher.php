b3a739c2543421b38d66833a962249cc591afd07
darrell-t <68108814+darrell-t@users.noreply.github.com>
2020-07-27 17:41:54 +0800

    viewDespatcher (Fixed Display Despatcher List)
    Registered despatchers will be shown in table

--- orders/viewDespatcher.php
+++ orders/viewDespatcher.php
@@ -1,9 +1,9 @@
 <?php include '../header.php'; ?>
 <title>Despatcher List</title>
-
 <div align="center">[<a href="orderMain.php">Previous Page</a>]
 <h1 align="center">Despatcher List</h1>
-<table border="1" align="center">
+<br>
+<table border="1" width="80%" style="text-align: center;">
 <tr>
 	<th>UserId</th>
 	<th>Name</th>
@@ -11,29 +11,26 @@
 	<th>Phone</th>
 	<th>Email</th>
 </tr>
-<tr>
-	<?php
-	//$link = mysqli_connect("localhost","root","","ubung");
-	$link = mysqli_connect("localhost", "root", "", "", "3306");
-	$select = "select * from user WHERE Role='Despatcher' ";
-	$run = mysqli_query($link, $select);
 
-	while($row = mysqli_fetch_array($run)){
-		$Id 			= $row['Id'];
-		$Name			= $row['UserName'];
-		$Address    	= $row['Address'];
-		$Phone			= $row['Phone'];
-		$Email			= $row['Email'];
-	?>
-	<td align="center"><?php echo $Id;?></td>
-	<td align="center"><?php echo $Name;?></td>
-	<td align="center"><?php echo $Address;?></td>
-	<td align="center"><?php echo $Phone;?></td>
-	<td align="center"><?php echo $Email;?></td>
-</tr>
-	<?php }  ?>
+<?php 
+	$conn = mysqli_connect("localhost", "root", "", "ubung");
+	$sql = "SELECT * FROM user WHERE Role='Despatcher'";
+	$result = $conn->query($sql);
+	if ($result -> num_rows > 0){
+		while($row = $result -> fetch_assoc()){
+?>
+	<td><?php echo $row['Id']; ?></td>	
+	<td><?php echo $row['UserName']; ?></td>	
+	<td><?php echo $row['Address']; ?></td>	
+	<td><?php echo $row['Phone']; ?></td>	
+	<td><?php echo $row['Email']; ?></td>	
+<?php		
+	}
+		}	
+?>
 </table>
-
+<br>
+<br>
 <form action="orderMain.php" method="post">
 <button class="v_btn" type="submit">Go to Main Page</button>
 </form>
