7fe40cf2b387f2978ada0e294a4be9e645010a82
darrell-t <68108814+darrell-t@users.noreply.github.com>
2020-07-28 02:17:18 +0800

    Revert "Revert "viewservices.php (Dispatcher can view own services)""
    This reverts commit d567bf50080da9c05cb35016211c7f4eabaca1da.

--- despatcher/viewservices.php
+++ despatcher/viewservices.php
@@ -2,52 +2,49 @@
 ob_start();
 ?>
 <?php include '../header.php';
-$ll = $_SESSION['username'];?>
-<h2>My Services</h2>
-<?php 
-    $query = "SELECT * FROM user WHERE username = '$ll'; " or die(mysqli_connect_error());
-    $result = mysqli_query($link, $query);
-  
-    while ($row = mysqli_fetch_array($result)) {
-    $a = $row["Id"];
+$username = $_SESSION['username'];?>
+<style>
+table, td, th {  
+  border: 1px solid #ddd;
+  text-align: center;
 }
-?>
-<?php 
-    $sql = "SELECT dispatcherservice.DispatcherId,service.Name, user.UserName FROM service,dispatcherservice, user WHERE dispatcherservice.DispatcherId = '$a' AND user.Id = '$a'; " or die(mysqli_connect_error());
-    if($result = mysqli_query($link, $sql)){
-        if(mysqli_num_rows($result) > 0){
-            echo "<table border='1'>";
+
+table {
+  border-collapse: collapse;
+  width: 80%;
+}
+
+th, td {
+  padding: 15px;
+}
+</style>
+<h2>My Services</h2>
+<table>
+    <tr>
+        <th>Dispatcher ID</th>
+        <th>Service</th>
+        <th>Username</th>
+        <th>Action</th>
+    </tr>
+<?php
+    $query = "SELECT * FROM user WHERE UserName = '$username';" or die(mysqli_connect_error());
+    $result = mysqli_query($link, $query);
+    while ($row = mysqli_fetch_array($result)){
+        $userID = $row['Id'];
+    }
+    $query2 = "SELECT * FROM dispatcherservice INNER JOIN service ON dispatcherservice.ServiceId = service.Id INNER JOIN user ON dispatcherservice.DispatcherId = user.Id";
+    $result = $link->query($query2);
+    if (mysqli_num_rows($result)) {
+        while ($row = mysqli_fetch_assoc($result)){
             echo "<tr>";
-            echo "<th>DispatcherId</th>";
-            echo "<th>Service Name</th>";			
-            echo "<th>Username</th>";
+            echo "<td>" . $row['DispatcherId'] . "</td>";
+            echo "<td>" . $row['Name'] . "</td>";
+            echo "<td>" . $row['UserName'] . "</td>";
+            echo "<td><button onclick='window.location.href=\"updateservices.php?id=" . $row['DispatcherId'] . "&sid=" . $row['ServiceId'] . "\"'>Edit</button>";
             echo "</tr>";
-
-        	while ($row = mysqli_fetch_array($result)) {
-	            echo "<tr>";
-		        echo "<td>" . $row['DispatcherId'] . "</td>";
-                echo "<td>" . $row['Name'] . "</td>";
-		    	echo "<td>" . $row['UserName'] . "</td>";
-                echo "</tr>";
-                echo' <td style="background-color:purple">
-                <button onclick="window.location.href = \'updateservice.php?id=' . $row["DispatcherId"] . '\';">Edit</button>
-				<button onclick="window.location.href = \'deleteservice.php?id=' . $row["DispatcherId"] . '\';">Delete</button>
-                </td>
-                </tr>                 
-                <tr>
-                    <td colspan = "5" style="background-color:grey">&nbsp;</td>
-                </tr>   ';        
-            }
-            echo "</table>";
-            mysqli_free_result($result);
-        } else {
-            echo "No Services Added Yet";
-            header( "refresh:2;url=services.php" );
-            ob_end_flush();
-
         }
-    } else {
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
     }
 ?>
+</table>
 <?php include '../footer.php';?>
+
