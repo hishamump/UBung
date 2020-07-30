83a96f17b7d235325963a5a7fd1d200e5bd63df7
Hisham <49261241+hishamump@users.noreply.github.com>
2020-07-30 13:06:41 +0800

    Fix delete confirmation message

--- despatcher/services.php
+++ despatcher/services.php
@@ -31,7 +31,7 @@ $result = mysqli_query($link, $query);
             <tr>
               <td><?php echo $row["Name"] ?></td>
               <td><button class="btn btn-primary" onclick="window.location.href = 'updateservice.php?id=<?php echo $row["Id"] ?>';">Edit</button></td>
-              <td><button class="btn btn-danger" id="btn-confirm" onclick="window.location.href = 'deleteservice.php?id=<?php echo $row["Id"] ?>';">Delete</button></td>
+              <td><button class="btn btn-danger btn-confirm" data-id="<?php echo $row["Id"] ?>" onclick="">Delete</button></td>
             </tr>
           <?php } ?>
         </tbody>
@@ -50,4 +50,47 @@ if (!$result) {
 mysqli_close($link);
 ?>
 
+<div class="modal fade" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="mi-modal">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" tabindex="-1" id="myModalLabel">Confirm Delete!</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" id="modal-btn-si">Yes</button>
+        <button type="button" class="btn btn-primary" id="modal-btn-no">No</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <?php include '../footer.php'; ?>
+
+<script>
+  var modalConfirm = function(callback) {
+    $(".btn-confirm").on("click", function() {
+      $("#mi-modal").modal('show');
+
+      var id = $(this).data('id');
+
+      $("#modal-btn-si").on("click", function() {
+        callback(true, id);
+        $("#mi-modal").modal('hide');
+      });
+
+      $("#modal-btn-no").on("click", function() {
+        callback(false);
+        $("#mi-modal").modal('hide');
+      });
+    });
+  };
+
+  modalConfirm(function(confirm, id) {
+    if (confirm) {
+      window.location.href = 'deleteservice.php?id=' + id;
+    } else {
+
+    }
+  });
+</script>
